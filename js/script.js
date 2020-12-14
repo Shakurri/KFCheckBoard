14c53d475d8c606b3b77be3b078fa858cc1c2769
Shakurri <puz,show40716@gmail.com>
2020-12-15 06:04:36 +0900

    first

---
+++ js/script.js
@@ -0,0 +1,11 @@
+$(function(){
+  var cardNum=22;
+  
+  $('.cardImg').on('touchstart',function () {
+  	if($(this).hasClass('add_dark')){
+      $(this).removeClass('add_dark');
+    }else{
+      $(this).addClass('add_dark');
+    }
+  });
+});
