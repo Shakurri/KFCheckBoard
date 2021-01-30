89e1c0f70f2360558f70312f2ef13fb92ff93110
Sho_Shinoda <puz.show40716@gmail.com>
2021-01-30 22:58:00 +0900

    relDisable

--- js/script.js
+++ js/script.js
@@ -179,21 +179,3 @@ $(function(){
   });
 
 });
-
-
-
-(function() {
-   // スクロールを禁止する関数
-   function noScroll(event) {
-     event.preventDefault();
-   }
-  // スクロール禁止(SP)
-  document.addEventListener('touchmove', noScroll, { passive: false });
-  // スクロール禁止(PC)
-
-  document.addEventListener('mousewheel', noScroll, { passive: false });
-  // スクロール禁止を解除(SP)
-  document.removeEventListener('touchmove', noScroll, { passive: false });
-  // スクロール禁止を解除(PC)
-  document.removeEventListener('mousewheel', noScroll, { passive: false });
-})();
